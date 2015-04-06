6d230636f08ae1494f41055fe5376c7e85235ff3
Sean Thomas <sthomas93@ufl.edu>
2015-04-06 13:18:47 -0400

    Added global definitions to turtle.h

--- turtle.h
+++ turtle.h
@@ -1,3 +1,67 @@
-// Table of Commands
-// Global Variables
-// Global Structures
+/*******************************************************************************
+ *   turtle.h contains all data structures and Constants for the turtle shell  *
+ *******************************************************************************
+*/
+ 
+#ifndef TURTLE_H
+#define TURTLE_H
+
+
+#define MAXCMDS 50
+#define MAXARGS 300
+#define MAXALIAS 100
+#define MAXCACHE 100
+#define MAXPATH 50
+
+#define OK 0
+#define SYSERR 1
+#define OLD_ERR 3 
+#define SYSCALLERR -1
+
+#define BADFD -2
+#define EOFILE 2
+
+// For Aliasing
+#define THE_ONLY_ONE 1
+#define FIRST 2
+#define LAST 3
+
+/* Command line arguments structure */
+ typedef struct comargs
+ {
+     char   *args[MAXARGS];
+ }  ARGTAB;
+
+/* command line structure */
+ typedef struct com
+ {
+     char   *comname;
+     int     remote;
+     int     infd;
+     int     outfd;
+     int     nargs;
+     ARGTAB *atptr;
+ }  COMMAND;
+
+/* Alias Structure */
+ struct alias
+ {
+     int    used;
+     char  *alname;
+     char  *alstring;
+ };
+
+/* cache table structure */
+ typedef struct cache
+ {
+     char   *cmd;
+     char   *hostport;
+ }  CACHE;
+
+/* externals */
+ extern struct alias aliastab[];
+ extern COMMAND      comtab[];
+ extern CACHE        cachetab[];
+ extern char        *pathtab[];
+
+#endif
